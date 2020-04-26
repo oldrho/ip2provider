f2977eec0894613fbe486853b83fe1a05ea9f7a3
oldrho <oldrho@oldrho.com>
2020-04-26 15:15:18 -0700

    Added update() for AWS

--- lists/aws.py
+++ lists/aws.py
@@ -1,5 +1,6 @@
 import ipaddress
 import json
+import requests
 
 # https://ip-ranges.amazonaws.com/ip-ranges.json
 
@@ -11,26 +12,53 @@ def check(ips):
 	results = []
 
 	# Load json data
-	with open("data/aws.json", "r") as f:
-		data = json.loads(f.read())
+	with open("data/aws.txt", "r") as f:
+		data = f.read().splitlines()
 		f.close()
 
-	def loop_ips(group):
-		# Loop through each IP
+	# Loop each route
+	for route in data:
+		(cidr,provider,service,region) = route.split()
+
+		# Loop each IP
 		for ip in ips:
-			cidr = group['ip_prefix'] if 'ip_prefix' in group.keys() else group['ipv6_prefix']
 			if ipaddress.ip_address(ip) in ipaddress.ip_network(cidr):
 				results.append({
 					'ip': ip,
-					'provider':'aws',
-					'service':group['service'],
-					'region':group['region']
+					'provider': provider,
+					'service': service,
+					'region': region
 					})
 
-	# Loop through all services
-	for group in data['prefixes']:
-		loop_ips(group)
-	for group in data['ipv6_prefixes']:
-		loop_ips(group)
-
 	return results
+
+
+
+def update():
+	results = []
+
+	response = requests.get('https://ip-ranges.amazonaws.com/ip-ranges.json')
+
+	if response.status_code != 200:
+		return False
+
+	data = json.loads(response.text)
+
+	def process_json(section):
+		nonlocal results,data
+
+		for group in data[section]:
+			cidr = group['ip_prefix'] if 'ip_prefix' in group.keys() else group['ipv6_prefix']
+
+			results.append("%s %s %s %s" % (cidr, 'aws', group['service'], group['region']))
+
+	process_json('prefixes')
+	process_json('ipv6_prefixes')
+
+
+	# Write to file
+	with open('data/aws.txt', 'w') as f:
+		f.write("\n".join(results))
+		f.close()
+
+	return len(results)
