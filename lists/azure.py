a36f48aa735011398764233e4838ad27b7db3ae7
oldrho <oldrho@oldrho.com>
2020-04-26 16:23:10 -0700

    Added update() for Azure

--- lists/azure.py
+++ lists/azure.py
@@ -1,5 +1,6 @@
 import ipaddress
 import json
+import requests
 
 # https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_20200420.json
 # https://download.microsoft.com/download/6/4/D/64DB03BF-895B-4173-A8B1-BA4AD5D4DF22/ServiceTags_AzureGovernment_20200420.json
@@ -8,37 +9,82 @@ def check(ips):
 	if isinstance(ips, str):
 		ips = [ips]
 
-	files = ['data/azure.json','data/azure-gov.json']
-
 	# Initialize results
 	results = []
 
+	def check_file(file):
+		nonlocal results
 
-	# Load json data
-	for file in files:
+
+		# Load json data
 		with open(file, "r") as f:
-			data = json.loads(f.read())
+			data = f.read().splitlines()
 			f.close()
 
-		# Loop through service groups
-		for group in data['values']:
-			try:
-				service = group['properties']['systemService']
-				region = group['properties']['region']
+		# Loop each route
+		for route in data:
+			(cidr,provider,service,region) = route.split()
 
-				# Loop through CIDR addresses
-				for cidr in group['properties']['addressPrefixes']:
+			# Loop each IP
+			for ip in ips:
+				if ipaddress.ip_address(ip) in ipaddress.ip_network(cidr):
+					results.append({
+						'ip': ip,
+						'provider': provider,
+						'service': service,
+						'region': region
+						})
 
-					# Loop through target IPs
-					for ip in ips:
-						if ipaddress.ip_address(ip) in ipaddress.ip_network(cidr):
-							results.append({
-								"ip": ip,
-								"provider":"azure",
-								"service":service,
-								"region":region
-								})
-			except:
-				True # NOOP
+	check_file('data/azure.txt')
+	check_file('data/azure-gov.txt')
 
 	return results
+
+
+def update():
+	results = []
+
+	def update_azure(provider, url, file):
+		nonlocal results
+
+
+		# Get JSON data from Azure
+		response = requests.get(url)
+		if response.status_code != 200:
+			return False
+		data = json.loads(response.text)
+
+
+		# Process service groups
+		for group in data['values']:
+			service = group['properties']['systemService']
+			region = group['properties']['region']
+
+			if not service or not region:
+				continue
+
+			for cidr in group['properties']['addressPrefixes']:
+				results.append("%s %s %s %s" % (cidr, provider, service, region))
+
+
+		# Write to file
+		with open(file, 'w') as f:
+			f.write("\n".join(results))
+			f.close()
+
+		return len(results)
+
+
+
+	count = 0
+	clouds = [
+		['azure', 'https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_20200420.json', 'data/azure.txt'],
+		['azure-gov', 'https://download.microsoft.com/download/6/4/D/64DB03BF-895B-4173-A8B1-BA4AD5D4DF22/ServiceTags_AzureGovernment_20200420.json', 'data/azure-gov.txt'],
+	]
+
+	for cloud in clouds:
+		(provider, url, file) = cloud
+
+		count += update_azure(provider, url, file)
+
+	return count
